658c778997e75f5161692c99fb24bbcb4d0a0323
Dennison Williams <dennison.williams@stjamesinfirmary.org>
2023-02-20 10:59:17 -0800

    Corecting env variables to use for creating an application user

--- mongo-init.d/10_create-sji-bdl-user.sh
+++ mongo-init.d/10_create-sji-bdl-user.sh
@@ -2,14 +2,14 @@
 #read -r -d '' JSON <<- EJSON
 cat << EJSON > /tmp/sji-bdl-createUser.js
 db.createUser( {
-	user: "${MONGO_INITDB_USERNAME:-sji-bdl}",
-	pwd: "${MONGO_INITDB_PASSWORD:-sji-bdl}",
+	user: "${MONGO_USERNAME:-sji-bdl}",
+	pwd: "${MONGO_PASSWORD:-sji-bdl}",
 	roles: [ {
 		role: "readWrite",
-		db: "${MONGO_INITDB_DATABASE:-sji-bdl}"
+		db: "${MONGO_DB:-sji-bdl}"
 	} ]
 } )
 EJSON
 
-"${mongo[@]}" "$MONGO_INITDB_DATABASE" /tmp/sji-bdl-createUser.js
+"${mongo[@]}" "$MONGO_DB" /tmp/sji-bdl-createUser.js
 rm /tmp/sji-bdl-createUser.js
