999b4e5344f658c73d75fbd166ecceb5d3c0d366
Dennison Williams <dennison.williams@stjamesinfirmary.org>
2023-03-03 17:22:19 -0800

    simplifying the db backup and allowing for more configuration from .env

--- backup-mongodb.sh
+++ backup-mongodb.sh
@@ -2,7 +2,7 @@
 # This can be run from cron daily with the following entry
 # 0 * * * * /path/backup-mongodb.sh
 dir="$( dirname ${BASH_SOURCE[0]})/"
-env="${dir}/.env"
+env="${dir}.env"
 export $(grep -v '^#' $env | xargs)
 db="${MONGO_DB:=sji-bdl}"
 user="${MONGO_USERNAME:=sji-bdl}"
@@ -10,9 +10,5 @@ pass="${MONGO_PASSWORD:=sji-bdl}"
 date=`date "+%H"`
 long_date=`date "+%Y%m%d%H"`
 backup_name="/data/db/${db}.${date}"
-temp_local_backup_name="${dir}${db}.${date}"
-docker exec -it db mongodump --host=localhost --port=27017 --db=$db --username=$user --password=$pass -o $backup_name
-docker cp db:${backup_name} $temp_local_backup_name
-docker exec -it db rm -rf $backup_name
-tar -zcf ${temp_local_backup_name}.${long_date}.tgz $temp_local_backup_name
-rm -rf $temp_local_backup_name
+local_backup_name="${LOCAL_PATH}/${db}"
+/usr/bin/docker exec db sh -c "exec mongodump -d ${db} -u ${user} --password=${pass} --archive" > $local_bakup_name
